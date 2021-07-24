378a136322a574a9685b46a8cad7276083bcab9c
Antonio <atcarter005@gmail.com>
2021-07-24 17:34:34 -0500

    updated lab

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,41 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    # byebug
+    @recipe = Recipe.new(recipe_params)
+    # @ingredient1 = Ingredient.new(name: )
+    # @ingredient2 = Ingredient.new(recipe_params["ingredients_attributes"].to_h[1])
+    # byebug
+    # @recipe.ingredients << @ingredient1
+    # @recipe.ingredients << @ingredient2
+    if @recipe.save
+      @recipe.ingredients.compact
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
   end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+    # params.require(:recipe)
+  end
+
+  # def ingredients_hash
+  #   recipe_params["ingredients_attributes"].to_h
+  # end
+
+  # def ing_name
+  #   ingredients_array["0"]
+  # end
+
+  # def ing_quantity
+
+  # end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,31 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(attributes)
+    attributes.values.each do |a|
+      i = self.ingredients.build(a)
+    end
+  end
+
+  # def ingredients_attributes
+  #   self.ingredients.map do |ingredient|
+  #     ingredient
+  #   end
+  # end
+
+  # def ingredient_attrs=(hash)
+  #   array = hash["recipe"]["ingredient_attributes"]
+  #   array.each do |a|
+  #     ingredient = self.ingredients.create(name: a["name"], quantity: a["quantity"])
+  #   end
+  # end
+
+  # def ingredient_attrs
+  #   self.ingredients.map do |ingredient|
+  #     # byebug
+  #     ingredient.name
+  #     ingredient.quantity
+  #   end
+  # end
 end
