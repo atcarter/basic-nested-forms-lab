c1cbba247d4c9145513c0dff36958f64955b2ffc
Antonio <atcarter005@gmail.com>
2021-07-24 17:35:49 -0500

    updated lab

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,11 +16,7 @@ class RecipesController < ApplicationController
   def create
     # byebug
     @recipe = Recipe.new(recipe_params)
-    # @ingredient1 = Ingredient.new(name: )
-    # @ingredient2 = Ingredient.new(recipe_params["ingredients_attributes"].to_h[1])
-    # byebug
-    # @recipe.ingredients << @ingredient1
-    # @recipe.ingredients << @ingredient2
+
     if @recipe.save
       @recipe.ingredients.compact
       redirect_to recipe_path(@recipe)
@@ -32,18 +28,5 @@ class RecipesController < ApplicationController
   private
   def recipe_params
     params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
-    # params.require(:recipe)
   end
-
-  # def ingredients_hash
-  #   recipe_params["ingredients_attributes"].to_h
-  # end
-
-  # def ing_name
-  #   ingredients_array["0"]
-  # end
-
-  # def ing_quantity
-
-  # end
 end
